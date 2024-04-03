364d34c52ef7c6ca1050c8ba2c0859c43139f6a0
David Stewart <stewart9376@hotmail.com>
2024-04-03 21:29:59 +0100

    add partials to folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.scss";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
